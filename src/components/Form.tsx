94e3f27fbb870ced1795ce81d391a91a3df0f1f4
kumamoto yusuke <y.kuma1229@gmail.com>
2023-03-31 10:23:34 +0900

    Feature/create form (#5)
    * 要素追加
    
    * Schema修正
    
    ---------
    
    Co-authored-by: YusukeKumamoto <yusuke.kumamoto@relic.co.jp>

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,15 @@
 import {
   Button,
+  ButtonGroup,
+  Container,
   FormControl,
   FormErrorMessage,
+  FormHelperText,
   FormLabel,
+  Heading,
   Input,
+  Select,
+  VStack,
 } from '@chakra-ui/react'
 import { zodResolver } from '@hookform/resolvers/zod'
 import type { SubmitHandler } from 'react-hook-form'
@@ -11,9 +17,17 @@ import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 
 const Schema = z.object({
-  name: z.string().nonempty({ message: 'This is required' }).min(4, {
-    message: 'Minimum length should be 4',
+  name: z.string().nonempty({ message: '必須項目です。' }).min(4, {
+    message: '4文字以上入力してください。',
   }),
+  email: z
+    .string()
+    .nonempty({ message: 'メールアドレスを入力してください。' })
+    .email({ message: '入力形式がメールアドレスではありません。' }),
+  country: z
+    .string()
+    .transform((value) => parseInt(value, 10))
+    .refine((value) => !!value, { message: '１つの国を選択してください。' }),
 })
 
 type NameType = z.infer<typeof Schema>
@@ -35,18 +49,63 @@ export default function HookForm() {
       }, 3000)
     })
 
+  const contries = [
+    { value: '1', name: 'United Arab Emirates' },
+    { value: '2', name: 'Nigeria' },
+  ]
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <FormControl isInvalid={!!errors.name}>
-        <FormLabel htmlFor="name">First name</FormLabel>
-        <Input id="name" placeholder="name" {...register('name')} />
-        <FormErrorMessage>
-          {errors.name && errors.name.message}
-        </FormErrorMessage>
-      </FormControl>
-      <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
-        Submit
-      </Button>
-    </form>
+    <Container maxW="container.sm">
+      <Heading as="h1" mb="24px">
+        RHF + Zod Form
+      </Heading>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <VStack spacing="16px">
+          <FormControl isInvalid={!!errors.name}>
+            <FormLabel htmlFor="name">Name</FormLabel>
+            <Input id="name" placeholder="name" {...register('name')} />
+            <FormErrorMessage>
+              {errors.name && errors.name.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={!!errors.email}>
+            <FormLabel htmlFor="email">Email address</FormLabel>
+            <Input
+              id="email"
+              placeholder="email"
+              type="email"
+              {...register('email')}
+            />
+            <FormErrorMessage>
+              {errors.email && errors.email.message}
+            </FormErrorMessage>
+            <FormHelperText>We&lsquo;ll never share your email.</FormHelperText>
+          </FormControl>
+          <FormControl isInvalid={!!errors.country}>
+            <FormLabel htmlFor="country">Country</FormLabel>
+            <Select
+              id="country"
+              placeholder="Select country"
+              {...register('country')}
+            >
+              {contries.map((country, index) => (
+                <option key={index} value={country.value}>
+                  {country.name}
+                </option>
+              ))}
+            </Select>
+            <FormErrorMessage>
+              {errors.country && errors.country?.message}
+            </FormErrorMessage>
+          </FormControl>
+        </VStack>
+        <ButtonGroup spacing="6" mt={4}>
+          <Button variant="outline">Reset</Button>
+          <Button colorScheme="teal" isLoading={isSubmitting} type="submit">
+            Submit
+          </Button>
+        </ButtonGroup>
+      </form>
+    </Container>
   )
 }
